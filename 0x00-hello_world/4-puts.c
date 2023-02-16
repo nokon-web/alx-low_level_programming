220b521fa47fcc80d157b277d9464bc5230b4a58
nokon <nokon@gmail.com>
2023-02-16 13:10:31 -0800

    all in one

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/*
+ * put Programming is like building a multilingual puzzle
+ *return 0
+ */
+int main(void){
+	put("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
+
+
