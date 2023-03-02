675940813ee27aa2a5b928bb5b6ace77671e9db0
nokon <nokon@gmail.com>
2023-03-02 07:41:51 -0800

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,33 +5,19 @@
  * _strcmp - function that compares two strings.
  * @s1: string 1
  * @s2: string 2
- * return: integer
+ * return: s1[i]-s2[i]
  */
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
-	int j = 0;
-	int count1 = 0;
-	int count2 = 0;
 
-	while (s1[i] != '\0')
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		count1 += 1;
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
 		i++;
 	}
-	while (s2[j] != '\0')
-	{
-		count2 += 1;
-		j++;
-	}
-	if (count1 < count2)
-	{
-		return (-15);
-	}
-	else if (count1 > count2)
-	{
-		return (15);
-	}
-	else
-		return (0);
+	return (0);
 }
