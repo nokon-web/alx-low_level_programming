45bb7ad21e3aed053101a4847794773edec672d8
nokon <nokon@gmail.com>
2023-03-22 04:01:57 -0700

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,11 +10,14 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	size_t i;
 
-	for (i = 0; i < size; i++)
-	{
-		(*action)(array[i]);
-	}
+if (array == NULL || action == NULL)
+return;
+
+while (size-- > 0)
+{
+action(*array);
+array++;
+}
 
 }
